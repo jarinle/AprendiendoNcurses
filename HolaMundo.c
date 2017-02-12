46df858344a0a2026698fee6b789dffaa0fafe13
CC3 Student <jarin.lima@galileo.edu>
2017-02-12 13:39:02 -0600

    Primer commit, holamundo

---
+++ HolaMundo.c
@@ -0,0 +1,16 @@
+/*
+    Instalar ncurses
+    sudo apt-get update
+    sudo apt-get install ncurses-dev
+*/
+#include <stdio.h>
+#include <ncurses.h>
+int main(int argc, char const *argv[]) {
+  initscr();//Inicializo el modo curses
+  printw("Hola Mundo\n"); //Imprimo en pantalla temporal no visible
+  refresh();//Refresco pantalla visible con Hola Mundo
+  getch();//Espero un caracter
+  endwin();//Cierro modo ncurses
+
+  return 0;
+}
